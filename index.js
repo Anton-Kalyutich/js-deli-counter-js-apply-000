0212f84c01d5dba12642c1916c93d289111601ab
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-06-23 21:28:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function nowServing(line) {
 
 function currentLine(line) {
   if (line.length === 0) {
-    return "The line is currently empty."
+    return "The line is currently empty.";
   }
   
   var lineString = "The line is currently: "
